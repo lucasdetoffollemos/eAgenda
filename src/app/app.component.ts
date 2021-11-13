b86041d89b529443a9cb8c1f91fa0b80da96e25c
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-11-13 14:30:04 -0300

    exemplos de Observable e promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+
 import { ITarefaService } from './shared/interfaces/ITarefaService';
 
 @Component({
@@ -12,8 +14,9 @@ export class AppComponent {
   registroSelecionado: number
 
   constructor(private servicoModal: NgbModal, @Inject('ITarefaServiceToken') private servico: ITarefaService) {
+    //this.minhaPromise('Luscas').then(result => console.log(result)).catch((erro)=> console.log(erro));
     
-    
+    this.minhaObsarvable('Lucas').subscribe(result => console.log(result), erro => console.log(erro))
   }
   abrirConfirmacao(modal:any){
       this.servicoModal.open(modal).result.then((resultado)=>{
@@ -22,5 +25,35 @@ export class AppComponent {
         }
         
       })
+  }
+
+  minhaPromise(nome:string):Promise<string>{
+    return new Promise((resolve, reject)=>{
+      if(nome == 'Lucas'){
+        setTimeout(()=>{
+          resolve('Seja bem vindo ' + nome)
+        }, 2000)
+      }
+      else{
+        reject('Errouuu')
+      }
+    })
+  }
+
+  minhaObsarvable(nome:string): Observable<string>{
+    return new Observable(subscriber=>{
+
+      if(nome == 'Lucas'){
+        subscriber.next('Olá')
+        subscriber.next('olá de novo')
+        setTimeout(()=>{
+          subscriber.next('Olá com delay')
+        }, 3000)
+      }
+      else{
+        subscriber.error('Ocorreu um erro')
+      }
+      
+    })
   }
 }
